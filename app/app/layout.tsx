74e939ef74f4aa7a9124ff78b44962345c02fc09
thorwebdev <thor@supabase.io>
2024-05-20 12:09:44 +0800

    chore: update metadata.

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,20 +1,26 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { GeistSans } from 'geist/font/sans'
+import './globals.css'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000'
 
+// TODO: make dynamic
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  title: 'Live Racing Location Tracker',
+  description: 'Follow the Pastis Cup Race #3 Live!',
+  images: [
+    {
+      url: 'https://mtgabtavgnhlcmbeednt.supabase.co/storage/v1/object/public/images/og/og.JPG',
+    },
+  ],
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
@@ -24,5 +30,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  );
+  )
 }
