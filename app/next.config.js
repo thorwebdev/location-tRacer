4fcb814b537d3562a4bff343c56e0ee7d51e3e52
thorwebdev <thor@supabase.io>
2024-05-20 12:42:59 +0800

    chore: update image loader.

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mtgabtavgnhlcmbeednt.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/**',
+      },
+    ],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
